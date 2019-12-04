f80a4ebefe5ace70ef3a87976a3ea4cba5f82db1
dujiajun <dujiajun1994@163.com>
2019-12-04 20:44:05 +0800

    combine state

--- models/AttModel.py
+++ models/AttModel.py
@@ -409,26 +409,18 @@ class TopDownCore(nn.Module):
         super(TopDownCore, self).__init__()
         self.drop_prob_lm = opt.drop_prob_lm
 
-        self.att_lstm = nn.LSTMCell(opt.input_encoding_size + opt.rnn_size * 2, opt.rnn_size) # we, fc, h^2_t-1
-        self.lang_lstm = nn.LSTMCell(opt.rnn_size * 2, opt.rnn_size) # h^1_t, \hat v
+        self.att_lstm = nn.LSTMCell(opt.rnn_size, opt.rnn_size)
+        self.lang_lstm = nn.LSTMCell(opt.input_encoding_size+opt.rnn_size, opt.rnn_size)
         self.attention = Attention(opt)
 
     def forward(self, xt, fc_feats, att_feats, p_att_feats, state, att_masks=None):
-        prev_h = state[0][-1]
-        att_lstm_input = torch.cat([prev_h, fc_feats, xt], 1)
-
-        h_att, c_att = self.att_lstm(att_lstm_input, (state[0][0], state[1][0]))
-
-        att = self.attention(h_att, att_feats, p_att_feats, att_masks)
-
-        lang_lstm_input = torch.cat([att, h_att], 1)
-        # lang_lstm_input = torch.cat([att, F.dropout(h_att, self.drop_prob_lm, self.training)], 1) ?????
-
-        h_lang, c_lang = self.lang_lstm(lang_lstm_input, (state[0][1], state[1][1]))
-
-        output = F.dropout(h_lang, self.drop_prob_lm, self.training)
-        state = (torch.stack([h_att, h_lang]), torch.stack([c_att, c_lang]))
-
+        h = state[0][0]
+        c = state[1][0]
+        h, c = self.lang_lstm(torch.cat([fc_feats, xt], 1), (h, c))
+        att = self.attention(h, att_feats, p_att_feats, att_masks)
+        h, c = self.att_lstm(att, (h, c))
+        output = F.dropout(h, self.drop_prob_lm, self.training)
+        state = (torch.stack([h]), torch.stack([c]))
         return output, state
 
 
@@ -660,7 +652,7 @@ class Att2all2Model(AttModel):
 class TopDownModel(AttModel):
     def __init__(self, opt):
         super(TopDownModel, self).__init__(opt)
-        self.num_layers = 2
+        self.num_layers = 1
         self.core = TopDownCore(opt)
 
 class StackAttModel(AttModel):
